39494bb73f14bbe632e1a35a2b969756d8f6edcf
Umang Vyas <57181653+UGman-game@users.noreply.github.com>
2023-05-23 19:37:38 +0530

    Update braintumor_app.py

--- braintumor_app.py
+++ braintumor_app.py
@@ -6,52 +6,52 @@ from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.models import load_model
 from PIL import Image, ImageEnhance
  
-model = load_model('brain_tumor.h5')
- 
+model = tf.keras.models.load_model('brain_tumor.h5')
 
+# Define the class labels
+class_labels = ['pituitary', 'notumor', 'meningioma', 'glioma']
+
+# Function to preprocess the image
 def preprocess_image(image):
-     image = Image.open(image)
-     image = image.convert('RGB')
-     image = ImageEnhance.Brightness(image).enhance(1.2)
-     image = ImageEnhance.Contrast(image).enhance(1.2)
-     image = image.resize((128, 128))
-     image = np.array(image) / 255.0
-     image = np.expand_dims(image, axis=0)
-     return image
- 
-labels = ['glioma', 'meningioma', 'no tumor', 'pituitary']
- 
+    image = image.resize((128, 128))
+    image = img_to_array(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
 
-def app():
-# Set page background image
-    folder_name = "images"
-    file_name = "bg.jpg"
-    image_path = os.path.join(folder_name, file_name)
-    page_bg_img = '''
-       <style>
-       body {
-       background-image: url("data:image/png;base64,%s");
-       background-size: cover;
-       }
-       </style>
-    ''' % base64.b64encode(open(image_path, 'rb').read()).decode()
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    
-    st.title('Brain Tumor Classification')
-    st.write('This app can classify the type of brain tumor from an uploaded image.')
-    
-    name = st.text_input('Enter Patient name')
-    if name:
-        st.write('Patient Name : ' + name )
-    
-    uploaded_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
+# Streamlit app
+def main():
+    st.title("Brain Tumor Classification")
+
+    # Text input for patient's name
+    patient_name = st.text_input("Enter patient's name")
+
+    # File uploader for image input
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
-        image = preprocess_image(uploaded_file)
-        prediction = model.predict(image)
-        predicted_label = labels[np.argmax(prediction)]
-        st.write('Type of tumor:', predicted_label)
-        st.image(image[0], caption='Uploaded Image', use_column_width=True)
- 
-if __name__ == '__main__':
-     app()
+        # Display the uploaded image
+        image = load_img(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+
+        # Preprocess the image
+        preprocessed_image = preprocess_image(image)
+
+        # Make the prediction
+        prediction = model.predict(preprocessed_image)
+        predicted_index = np.argmax(prediction)
+        predicted_label = class_labels[predicted_index]
+
+        # Display the prediction
+        st.subheader("Prediction:")
+        if predicted_label == 'notumor':
+            st.write("No tumor detected")
+        else:
+            st.success(f"Tumor Detected: {predicted_label}")
+
+        # Display the patient's name
+        st.subheader("Patient's Name:")
+        st.write(patient_name)
+
+# Run the app
+if __name__ == "__main__":
+    main()
