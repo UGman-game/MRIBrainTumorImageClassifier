c2453e3f2b72b6dc4aad3b13e55aaf5d1f610683
Umang Vyas <57181653+UGman-game@users.noreply.github.com>
2023-06-19 16:25:02 +0530

    Update braintumor_app.py

--- braintumor_app.py
+++ braintumor_app.py
@@ -2,25 +2,57 @@ import streamlit as st
 import base64
 import os
 import numpy as np
-from tensorflow.keras.preprocessing.image import load_img
+import tensorflow as tf
+import pandas as pd
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.models import load_model
 from PIL import Image, ImageEnhance
- 
-model = tf.keras.models.load_model('brain_tumor.h5')
+
+model = tf.keras.models.load_model('/content/drive/MyDrive/Models/cnn_braintumor_model.h5')
 
 # Define the class labels
-class_labels = ['pituitary', 'notumor', 'meningioma', 'glioma']
+class_labels = ['pituitary', 'invalid', 'meningioma', 'glioma']
 
 # Function to preprocess the image
 def preprocess_image(image):
-    image = image.resize((128, 128))
+    image = image.resize((224, 224))
     image = img_to_array(image) / 255.0
     image = np.expand_dims(image, axis=0)
     return image
 
+# Function to apply blur to the background image
+def apply_blur(image):
+    blurred_image = cv2.GaussianBlur(image, (15, 15), 0)
+    return blurred_image
+
 # Streamlit app
 def main():
+    st.markdown(
+        """
+        <style>
+        .stApp {
+            background: url('https://st2.depositphotos.com/1518767/6951/v/600/depositphotos_69511347-stock-video-brain-spinning-on-black-background.jpg');
+            background-size: cover;
+        }
+        .stApp > div:first-child {
+            filter: blur(15px);
+        }
+        .result-box {
+            border: 2px solid white;
+            padding: 10px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+        }
+        .result-box h2 {
+            margin-top: 0;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
     st.title("Brain Tumor Classification")
+    st.subheader("A simple app to classify brain tumors")
 
     # Text input for patient's name
     patient_name = st.text_input("Enter patient's name")
@@ -28,25 +60,47 @@ def main():
     # File uploader for image input
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
+    # Add a submit button
+    submit_button = st.button("Submit")
+
+    # Add a clear button
+    clear_button = st.button("Clear")
+
     if uploaded_file is not None:
         # Display the uploaded image
         image = load_img(uploaded_file)
         st.image(image, caption="Uploaded Image", use_column_width=True)
 
-        # Preprocess the image
-        preprocessed_image = preprocess_image(image)
+        if submit_button:
+            # Preprocess the image
+            preprocessed_image = preprocess_image(image)
 
-        # Make the prediction
-        prediction = model.predict(preprocessed_image)
-        predicted_index = np.argmax(prediction)
-        predicted_label = class_labels[predicted_index]
+            # Add a progress bar
+            progress_bar = st.progress(0)
+            progress_text = st.empty()
 
-        # Display the prediction
-        st.subheader("Prediction:")
-        if predicted_label == 'notumor':
-            st.write("No tumor detected")
-        else:
-            st.success(f"Tumor Detected: {predicted_label}")
+            # Make the prediction
+            prediction = model.predict(preprocessed_image)
+            predicted_index = np.argmax(prediction)
+            predicted_label = class_labels[predicted_index]
+
+            # Update the progress bar
+            progress_bar.progress(100)
+            progress_text.text("Done!")
+
+            # Display the prediction
+            st.subheader("Result:")
+            with st.container():
+                with st.container() as result_box:
+                    st.markdown(
+                        f"<h2>{predicted_label}</h2>",
+                        unsafe_allow_html=True
+                    )
+                    for label, score in zip(class_labels, prediction[0]):
+                        st.write(f"- {label}: {score:.1f}")
+
+        if clear_button:
+            uploaded_file = None
 
         # Display the patient's name
         st.subheader("Patient's Name:")
